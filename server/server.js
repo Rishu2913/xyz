a6d461554d38eae29ef4ae4247f4237c24c07b13
Piyush-Samal <piyush.24scse1180167@galgotiasuniversity.ac.in>
2026-08-24 00:11:22 +0530

    used cors

--- server/server.js
+++ server/server.js
@@ -2,12 +2,14 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
