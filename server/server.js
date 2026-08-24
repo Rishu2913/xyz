d8ae3fad5575590e5ecd23ff3a2a8e43586dc130
Piyush-Samal <piyush.24scse1180167@galgotiasuniversity.ac.in>
2026-08-25 01:18:34 +0530

    hello

--- server/server.js
+++ server/server.js
@@ -25,10 +25,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: [
-            "http://localhost:5173",
-            "https://xyz-beta-eight.vercel.app/"
-        ],
+        origin: true,
         methods: ["GET", "POST"],
         credentials: true
     }
