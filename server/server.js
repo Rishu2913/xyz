3b3d34ea4f5c1969ce9d4ca4cca6f70d3530a45c
Piyush-Samal <piyush.24scse1180167@galgotiasuniversity.ac.in>
2026-08-25 00:18:04 +0530

    Allow production frontend CORS

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "https://xyz-beta-eight.vercel.app",
         methods: ["GET", "POST"]
     }
 });
@@ -33,7 +33,7 @@ const io = new Server(server, {
 connectDB();
 
 app.use(cors({
-    origin: "http://localhost:5173"
+    origin: "https://xyz-beta-eight.vercel.app"
 }));
 
 app.use(express.json());
